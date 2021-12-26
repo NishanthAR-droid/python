6b7bf5ad31c742f536355051e8ffe42c40f3466f
NishanthAR-droid <85539246+NishanthAR-droid@users.noreply.github.com>
2021-12-26 23:18:09 +0530

    Update StudentAdministrationSystem.py

--- StudentAdministrationSystem.py
+++ StudentAdministrationSystem.py
@@ -1,14 +1,3 @@
-# Student Management System
-"""
-Fields :- ['roll', 'name', 'age', 'email', 'phone']
-1. Add New Student
-2. View Students
-3. Search Student
-4. Update Student
-5. Delete Student
-6. Quit
-"""
- 
 import csv
 # Define global variables
 student_fields = ['roll', 'name', 'age', 'email', 'phone']
