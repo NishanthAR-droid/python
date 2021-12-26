7e10a46de5ca30fe6749ff10ad11b4938b21ca1d
NishanthAR-droid <85539246+NishanthAR-droid@users.noreply.github.com>
2021-12-26 22:51:39 +0530

    Add files via upload

---
+++ MostFrequet.py
@@ -0,0 +1,11 @@
+test_str = "MISSISSIPPI"
+  
+most_freq = {}
+  
+for i in test_str:
+    if i in most_freq:
+        most_freq[i] += 1
+    else:
+        most_freq[i] = 1
+
+print ("Count of all characters in MISSISSIPPI is :\n " +  str(most_freq))
