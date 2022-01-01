d1f473ee39b9440fa612189c1a0a4573d5c81653
NishanthAR-droid <85539246+NishanthAR-droid@users.noreply.github.com>
2022-01-01 12:16:33 +0530

    Delete AreaOfCircle_and_FileExtension.py

--- AreaOfCircle_and_FileExtension.py
+++
@@ -1,9 +0,0 @@
-#program to compute area of circle
-from math import pi
-r = float(input ("Input the radius of the circle : "))
-print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
-
-#extension program
-filename = input("Input the Filename: ")
-f_extns = filename.split(".")
-print ("The extension of the file is : " + repr(f_extns[-1]))
