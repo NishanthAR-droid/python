cad74509d11323a6619631ba2edd9a2a24631418
NishanthAR-droid <85539246+NishanthAR-droid@users.noreply.github.com>
2023-09-16 12:51:58 +0530

    Update setOperations.py

--- setOperations.py
+++ setOperations.py
@@ -1,14 +1,14 @@
 e = {1, 2, 3, 4, 5}
 n = {4, 5, 6, 7, 8}
 
-union_result = set1.union(set2)
+union_result = e.union(n)
 print("Union of e and n is:", union_result)
 
-intersection_result = set1.intersection(set2)
+intersection_result = e.intersection(n)
 print("Intersection of e and n is:", intersection_result)
 
-difference_result = set1.difference(set2)
+difference_result = e.difference(n)
 print("Difference of e and n is:", difference_result)
 
-symmetric_difference_result = set1.symmetric_difference(set2)
+symmetric_difference_result = e.symmetric_difference(n)
 print("Symmetric Difference of e and n is:", symmetric_difference_result)
