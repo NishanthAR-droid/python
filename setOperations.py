14b8dfff5d05c505da02c9b15fed55f49ed2c3ae
NishanthAR-droid <85539246+NishanthAR-droid@users.noreply.github.com>
2023-09-16 12:43:32 +0530

    Create setOperations.py

---
+++ setOperations.py
@@ -0,0 +1,14 @@
+e = {1, 2, 3, 4, 5}
+n = {4, 5, 6, 7, 8}
+
+union_result = set1.union(set2)
+print("Union of e and n is:", union_result)
+
+intersection_result = set1.intersection(set2)
+print("Intersection of e and n is:", intersection_result)
+
+difference_result = set1.difference(set2)
+print("Difference of e and n is:", difference_result)
+
+symmetric_difference_result = set1.symmetric_difference(set2)
+print("Symmetric Difference of e and n is:", symmetric_difference_result)
